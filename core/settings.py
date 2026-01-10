f39e6131a42bc5a93e5ccc7258373f0857607e8e
Harika67-coder285 <tirumalasettyharika4@gmail.com>
2026-01-10 22:44:22 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -130,8 +130,9 @@ CSRF_TRUSTED_ORIGINS = [
 
 # -------------------- EMAIL --------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'apikey'  # This is literally 'apikey'
+EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')  # Your SendGrid API key
+DEFAULT_FROM_EMAIL = 'SkillConnect <noreply@skillconnect.com>'
